cab4a658177d9b9b661ed7774e779548568b4e27
reviq97 <nway@o2.pl>
2023-03-14 01:00:26 +0100

    Prepared wsb-restaurant-models module for migration and created wsb-restaurant-dal

--- wsb-restaurant-api/Program.cs
+++ wsb-restaurant-api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using wsb_restaurant_models.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql("Host=localhost:5432;Database=postgres;Username=postgres;Password=admin", b => b.MigrationsAssembly("wsb-restaurant-api")));
 
 var app = builder.Build();
 
---
+++ wsb-restaurant-dal/Repository/ApplicationDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wsb_restaurant_models.EFMigration.Models.Entities;
+using wsb_restaurant_models.EFMigration.Models.LinkingTables;
+
+namespace wsb_restaurant_models.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<CustomerAddress> CustomerAddresses { get; set; }
+        public DbSet<CustomerContact> CustomerContacts { get; set; }
+        public DbSet<EmployeeAddress> EmployeeAddresses { get; set; }
+        public DbSet<EmployeeContact> EmployeeContacts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderPayment> OrderPayments { get; set; }
+        public DbSet<RestaurantAddress> RestaurantAddresses { get; set; }
+        public DbSet<RestaurantDish> RestaurantDishes { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
